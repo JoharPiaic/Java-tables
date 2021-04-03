d0805a48eed21133bc4f6c34f5b4e28950eb3f6f
Johar Ali <50805982+JoharPiaic@users.noreply.github.com>
2021-04-03 23:59:25 +0500

    java table

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // alert("Hello World")
- var b = 2;
+
+//  var b = 2;
+// for( a=1; a<=16; a++){
+//     document.write (b+ " * "+ a + " = " + " " +  a*b + " " + "</br>")
+// }
+
+
+
+var b = +prompt("Enter the table Number");
 for( a=1; a<=16; a++){
-    document.write (a+ " * "+ a + " = " + " " +  a*b + " " + "</br>")
+    document.write (b+ " * "+ a + " = " + " " +  a*b + " " + "</br>")
 }
